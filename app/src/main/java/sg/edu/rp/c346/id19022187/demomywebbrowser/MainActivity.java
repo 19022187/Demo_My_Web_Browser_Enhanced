576cf8ff408eac9f8d5262a60851371fa4c4ac86
19022187 <19022187@myrp.edu.sg>
2020-08-13 11:02:31 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19022187/demomywebbrowser/MainActivity.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.id19022187.demomywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etURL;
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etURL = findViewById(R.id.editTextURL);
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        wvMyPage.setWebViewClient(new WebViewClient());
+        wvMyPage.getSettings().setJavaScriptEnabled(true);
+        wvMyPage.getSettings().setAllowFileAccess(false);
+        wvMyPage.getSettings().setBuiltInZoomControls(true);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = etURL.getText().toString();
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
